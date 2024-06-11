dd0db7fa63505a386b01bad417cfed014491578f
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-06-12 01:12:35 +0800

    :memo: Add SO link

--- app/src/main/java/com/example/notificationblocker/MainActivity.kt
+++ app/src/main/java/com/example/notificationblocker/MainActivity.kt
@@ -11,6 +11,8 @@ import android.view.Menu
 import android.view.MenuItem
 import com.example.notificationblocker.databinding.ActivityMainBinding
 
+// https://stackoverflow.com/questions/72853917/how-to-use-notificationlistenerservice-to-block-notification-in-android
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
