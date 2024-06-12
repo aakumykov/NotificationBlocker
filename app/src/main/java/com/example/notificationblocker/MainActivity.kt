86aa2267a43f9b188ef9be43decc6c3b93353d87
ValentinVignal <valentin.vignal.dev@outlook.fr>
2024-06-13 00:56:15 +0800

    :sparkles: Add images

--- app/src/main/java/com/example/notificationblocker/MainActivity.kt
+++ app/src/main/java/com/example/notificationblocker/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,9 +32,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             NotificationBlockerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    GreetingText(
-                        message = "Happy Birthday Sam!",
-                        from = "From Emma",
+                    GreetingImage(
+                        message = stringResource(R.string.hello_you),
+                        from = stringResource(R.string.from_me),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -60,10 +65,31 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NotificationBlockerTheme {
-        GreetingText(message = "Hello you", from = "From me")
+        GreetingImage(message = "Hello you", from = "From me")
     }
 }
